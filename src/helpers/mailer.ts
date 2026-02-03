bdc6fa4021a171eb9256a694a842381b451d0348
Shivam Soni <130232913+sethshivam11@users.noreply.github.com>
2026-02-03 08:25:23 +0000

    Fixed mailer transport

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -5,6 +5,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
